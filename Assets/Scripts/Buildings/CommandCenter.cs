4d9d1b2edf2d4e6ca47857fd36e4c7b0822a5d3a
realdka13 <donovanmagney@yahoo.com>
2019-01-08 14:43:18 -0600

    Selectable Buildings
    Buildings are now selectable

--- Assets/Scripts/Buildings/CommandCenter.cs
+++ Assets/Scripts/Buildings/CommandCenter.cs
@@ -5,17 +5,6 @@ using UnityEngine.UI;
 
 public class CommandCenter : MonoBehaviour
 {
-
     [SerializeField]
     private GameObject commandCenterUI;
-    [SerializeField]
-    private GameObject selectedIcon;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        //Set Icon to Off by default
-        selectedIcon.SetActive(false);
-    }
-
 }
---
+++ Assets/Scripts/Selection/SelectableBuilding.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectableBuilding : MonoBehaviour
+{
+    private bool isSelected;
+
+    [SerializeField]
+    private GameObject selectedIcon;
+
+    private void Start()
+    {
+        //Set Icon to Off by default
+        isSelected = false;
+        selectedIcon.SetActive(false);
+    }
+
+    public bool OnClicked() //Is Toggle Button Held
+    {
+        if (isSelected)
+        {
+            OnDeselect();
+        }
+        else if (!isSelected)  //If it is not selected, select it
+        {
+            OnSelected();
+        }
+        return isSelected; //Tell if gameobject hit is selected or unselected
+    }
+
+    public void OnSelected()
+    {
+        isSelected = true;
+        UpdateSelectionSign();
+    }
+
+    public void OnDeselect()
+    {
+        isSelected = false;
+        UpdateSelectionSign();
+    }
+
+    public void UpdateSelectionSign()
+    {
+        if (isSelected)
+        {
+            selectedIcon.SetActive(true);
+        }
+        else if (!isSelected)
+        {
+            selectedIcon.SetActive(false);
+        }
+    }
+}

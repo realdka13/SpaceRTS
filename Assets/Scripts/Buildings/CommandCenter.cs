e8763af0067aeecd90a3920859069dbb324ce57d
realdka13 <donovanmagney@yahoo.com>
2019-01-08 12:52:22 -0600

    Other UI's and Command Center Started
    UIs for different unit types and a command center has been added. But still not selectable

---
+++ Assets/Scripts/Buildings/CommandCenter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CommandCenter : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject commandCenterUI;
+    [SerializeField]
+    private GameObject selectedIcon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Set Icon to Off by default
+        selectedIcon.SetActive(false);
+    }
+
+}
